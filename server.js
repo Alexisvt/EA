0c45a6a7c2a45602f44ee8622c1e51f47399907a
Alexis <alexisvt@gmail.com>
2015-02-15 19:10:36 -0600

    finish the routing

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 //Including projects files
 var routes = require('./lib/controllers/routes.js');
 var bodyParser = require('body-parser');
+var express = require('express');
 
 
 //Init server IIFE pattern
-var server = (require('express')());
+var server = express();
 
 //Setting other config for the server
 server.set('view engine', 'jade');
@@ -13,7 +14,22 @@ server.set('view cache', true);
 
 //Middlewares
 server.use(bodyParser.json());
-server.use(bodyParser.urlencoded(extended : true));
+server.use(bodyParser.urlencoded({extended : true}));
+
+//serving static files
+server.use(express.static(__dirname + "/public"));
 
 //Implementing MVC Pattern
 //Calling Controllers aka routing
+if( routes.registerRoutes !== undefined){
+  routes.registerRoutes(server);
+}
+else{
+  console.log('routes are undefined');
+}
+
+//Starting Server
+server.listen(server.get('port'), function () {
+  console.log('Express started on http:localhost:' + server.get('port') +
+             "; press Cotrl-C to terminate");
+});
