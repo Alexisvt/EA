59a01175bdec8faa3ed45db803bfd47e73d2b65f
Alexis <alexisvt@gmail.com>
2015-02-26 13:46:41 -0600

    adding extra controller

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,12 +1,29 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\store.js":[function(require,module,exports){
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\productListCtrl.js":[function(require,module,exports){
+module.exports = angular.module("storeApp")
+  .controller("productListCtrl", function () {
+
+  var selectedCategory = null;
+
+  $scope.selectCategory = function (newCategory) {
+    selectedCategory = newCategory;
+  };
+
+  $scope.categoryFilterFn = function (product) {
+    return selectedCategory == null || product.category == selectedCategory;
+  };
+});
+
+},{}],"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\store.js":[function(require,module,exports){
 var $ = require('jquery');
 var angular = require('angular');
 
 angular.module("storeApp", ["customFilters"]);
 
-require('./storeAppCtrl.js');
-require('./uniqueFltr.js');
-},{"./storeAppCtrl.js":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\storeAppCtrl.js","./uniqueFltr.js":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\uniqueFltr.js","angular":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\node_modules\\angular\\index.js","jquery":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\node_modules\\jquery\\dist\\jquery.js"}],"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\storeAppCtrl.js":[function(require,module,exports){
+require('./storeAppCtrl');
+require('./uniqueFltr');
+require('./productListCtrl');
+
+},{"./productListCtrl":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\productListCtrl.js","./storeAppCtrl":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\storeAppCtrl.js","./uniqueFltr":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\uniqueFltr.js","angular":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\node_modules\\angular\\index.js","jquery":"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\node_modules\\jquery\\dist\\jquery.js"}],"C:\\Users\\OSI-7\\Documents\\Test Enviroments\\ExpressAndAngularJs\\AngularJsFiles\\storeAppCtrl.js":[function(require,module,exports){
 module.exports = angular.module("storeApp").controller("storeAppCtrl", function ($scope) {
   $scope.data = {
     products: [
